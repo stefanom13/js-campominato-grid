9545511e222c5769ffe8d0ab45b51545c36d88b2
stefanom13 <stefanomancuso13@gmail.com>
2022-02-16 15:34:59 +0100

    inserimento option e collegamento js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('ciaooo');
+const buttonPlay = document.getElementById('buttonPlay')
+
+
+// salviamo il click nella variabile startgame
+const startGame = () => {
+    console.log('inizia il gioco');
+}
+
+buttonPlay.addEventListener('click', startGame)
